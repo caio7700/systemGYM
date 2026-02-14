0255f7f93576e29eca5dc315687383645672cc67
caio7700 <caiorocha113@gmail.com>
2026-02-14 15:46:15 -0300

    Criando o html da página de cadastro do Aluno

--- systemGym/src/main/java/com/systemgym/systemGym/controller/AlunoController.java
+++ systemGym/src/main/java/com/systemgym/systemGym/controller/AlunoController.java
@@ -1,15 +1,19 @@
 package com.systemgym.systemgym.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.systemgym.systemgym.entity.Aluno;
 
 @Controller
 @RequestMapping("admin/aluno")
 public class AlunoController {
 
     @GetMapping
-    public String teste() {
-        return "Parabéns! A rota /admin/aluno está funcionando e o servidor está ok.";
-    }
+public String exibirFormulario(Aluno aluno, Model model) {
+        model.addAttribute("msgm", "O servidor está ok e o Thymeleaf também!");
+        return "admin/formAluno";
+}
 }
